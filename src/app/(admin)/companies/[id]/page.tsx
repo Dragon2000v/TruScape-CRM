8dbb1fa1612cc524b2e54f1b0cf8653d683a43b0
mykolatsiapalo1 <shmatko064@gmail.com>
2024-09-28 18:07:39 +0300

    add new components

---
+++ src/app/(admin)/companies/[id]/page.tsx
@@ -0,0 +1,22 @@
+"use client"
+import React, {useEffect} from 'react';
+import  {notFound} from "next/navigation";
+import Header from '@/app/components/header';
+
+export interface PageProps {
+    params: { id: string };
+}
+
+export default function Page({ params }: PageProps) {
+    useEffect(()=>{
+        const  id = Number.parseInt(params.id);
+        if(Number.isNaN(id)){
+            notFound()
+        }
+    },[params.id]);
+    return (
+        <>
+            <Header>Company ({params.id})</Header>
+        </>
+    );
+}
