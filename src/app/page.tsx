c24f9e7086c9d73e7d9a52ee0bb628fd16fc43d8
Vitaly <shmatko064@gmail.com>
2024-09-29 01:04:14 +0300

    add navigation button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
-export default function Home() {
+'use client';
+import { useRouter } from 'next/navigation';
+
+const Home: React.FC = () => {
+  const router = useRouter();
+
+  const goToDashboard = () => {
+    router.push('/dashboard');
+  };
+
   return (
-    <main>
-      <h1 className="text-xl">Home page</h1>
+    <main className="flex items-center justify-center min-h-screen">
+      <button
+        onClick={goToDashboard}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full shadow-lg transition duration-300 ease-in-out"
+      >
+        Go to Dashboard
+      </button>
     </main>
   );
-}
+};
+
+export default Home;
